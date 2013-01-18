13d9e0504a3e7e4d9041f817395c423be7b777e8
hanyangrabbit <hanyang213@gmail.com>
2013-01-18 22:48:54 +0800

    add setup for installation.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hat',
+      description='Python wrapper for Hadoop MapReduce',
+      version='0.01',
+      author='Yang Han',
+      packages=['hat']
+      )
